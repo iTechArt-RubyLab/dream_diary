f6278c92de756570d20abb960c1b42c661e2b540
evrusha <uzaharau@gmail.com>
2023-05-03 21:18:55 +0300

    removed_image_from_dreams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_175705) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_175051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,7 +69,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_175705) do
     t.integer "duration"
     t.string "title"
     t.text "description"
-    t.string "image"
     t.text "tags"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ Category.create(name: 'Food', description: Faker::Food.description)
     duration: rand(0..24),
     title: Faker::Lorem.sentence(word_count: 3),
     description: Faker::Lorem.paragraph(sentence_count: 3),
-    image: Faker::Placeholdit.image(size: '150x150', format: 'jpeg', background_color: :random),
     tags: Faker::Hipster.words(number: rand(1..4)),
     user_id: User.first.id,
     category_id: Category.first.id
