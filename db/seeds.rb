19413b74fc84d085305bced3ba2d30afc6ab9041
Uladzislau Yeuhrafau <92786798+evrusha@users.noreply.github.com>
2023-05-12 08:04:44 +0000

    removed tags from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@ Category.create(name: 'Food', description: Faker::Food.description)
     duration: rand(0..24),
     title: Faker::Lorem.sentence(word_count: 3),
     description: Faker::Lorem.paragraph(sentence_count: 3),
-    tags: Faker::Hipster.words(number: rand(1..4)),
     user_id: User.first.id,
     category_id: Category.first.id
   )
