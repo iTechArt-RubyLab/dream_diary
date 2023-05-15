c7d8ec6285a6685e4602ccaa0e4e4009327baaf2
evrusha <uzaharau@gmail.com>
2023-05-15 22:48:48 +0300

    added_phase_names

--- app/services/moon_phase.rb
+++ app/services/moon_phase.rb
@@ -1,15 +1,25 @@
 module MoonPhase
   module_function
 
+  include Constants
+
   def phase(request)
-    coordinates = request.location.coordinates.join(',')
+    coordinates = request.local? ? MINSK_COORDINATES : request.location.coordinates.join(',')
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/#{coordinates}/today"
     params = {
       key: ENV['MOON_KEY'],
-      include: 'days',
+      include: 'timezone,days',
       elements: 'datetime,moonphase,sunrise,sunset,moonrise,moonset'
     }
-    location_data = request.location.data.symbolize_keys.slice(:city, :region, :country, :timezone)
-    HTTParty.get(url, query: params).parsed_response['days'].first.merge(location_data)
+    response = HTTParty.get(url, query: params).parsed_response.slice('timezone', 'days')
+    response['days'].each { |day| day['moonphase_name'] = represent_phase(day['moonphase']) }
+    response
+  end
+
+  def represent_phase(phase)
+    MOON_PHASES.each do |range, phase_name|
+      return phase_name if range === phase
+    end
+    'Invalid moon phase'
   end
 end
---
+++ lib/constants.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Constants
+  MINSK_COORDINATES = '53.893009,27.567444'
+  MOON_PHASES = {
+    0 => 'New moon',
+    0.01..0.24 => 'Waxing crescent',
+    0.25 => 'First quarter',
+    0.26..0.49 => 'Waxing gibbous',
+    0.5 => 'Full moon',
+    0.51..0.74 => 'Waning gibbous',
+    0.75 => 'Last quarter',
+    0.76..1 => 'Waning crescent'
+  }.freeze
+end
