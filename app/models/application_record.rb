969823032b06aceeaaf116a7fc77708c76b7b809
nkawru <133848876+nkawru@users.noreply.github.com>
2023-05-19 07:45:38 +0000

    cop_fixes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,9 +7,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   def purge_attached_files
     self.class.reflect_on_all_attachments.each do |reflection|
-      if reflection.macro.in?([:has_one_attached, :has_many_attached])
-        public_send(reflection.name).purge_later
-      end
+      public_send(reflection.name).purge_later if reflection.macro.in?(%i[has_one_attached has_many_attached])
     end
   end
 end
