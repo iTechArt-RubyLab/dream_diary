721b80cf4ab81e7869525a147cdb7b4b97496774
evrusha <uzaharau@gmail.com>
2023-05-24 11:49:11 +0300

    search_route_fixed

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,7 @@ Rails.application.routes.draw do
 
   resources :dreams do
     resources :comments
-    collection do
-      get 'search', to: 'dreams#search'
-    end
+    get 'search', on: :collection
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
