0fcc9d213f2aa1c62e1bffedb79d15907c424d61
evrusha <uzaharau@gmail.com>
2023-05-28 12:39:33 +0300

    updated_devise_sign_up_path_for

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,6 +8,10 @@ module Users
 
     protected
 
+    def after_inactive_sign_up_path_for(resource)
+      new_user_session_path
+    end
+
     def configure_sign_up_params
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email avatar])
     end
